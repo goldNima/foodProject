0eee4a5fb5c5f53cd57c5a5d061dbca57394a378
Nima <165915862+goldNima@users.noreply.github.com>
2025-02-18 08:53:05 -0800

    Update getApi-shop.tsx

--- src/app/getapi/getApi-shop.tsx
+++ src/app/getapi/getApi-shop.tsx
@@ -5,9 +5,6 @@ import { useDispatch, UseDispatch } from "react-redux";
 import { addToCart } from "../store/cartSlice";
 import Link from "next/link";
 
-import { useEffect } from "react";
-import AOS from "aos"
-import "aos/dist/aos.css"
 
 export const GetApiShop = ()=>{
 
@@ -17,18 +14,11 @@ export const GetApiShop = ()=>{
         dispatch(addToCart(product))
     }
 
-    useEffect (()=> {
-        AOS.init({
-            duraction : 1000,
-            once: true,
-        })
-    }, [])
-
     return(
         <div>
             <div className="container-shop">
                 {ApiShop.map((product)=>(
-                    <div data-aos="zoom-in" className="items-container" key={product.id}>
+                    <div className="items-container" key={product.id}>
                         <img src={product.image} alt="" className="item-image" />
                         <h3 className="name-items">{product.name}</h3>
                         <p className="price-item">{product.price} $</p>
