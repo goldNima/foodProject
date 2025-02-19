d53e574d5fb5f199662bee0c1b2c8f301d6140fe
Nima <165915862+goldNima@users.noreply.github.com>
2025-02-19 08:03:57 -0800

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,21 @@
 "use client"
 
 import Link from "next/link"
+import { useEffect , useState } from "react"
 
 function dashboard (){
 
-    const name = localStorage.getItem("name")
-    const lastName = localStorage.getItem("Lastname")
-    const email = localStorage.getItem("email")
-    const password = localStorage.getItem("password")
+    const [name , setName] = useState<string | null>(null);
+    const [lastName , setLastName] = useState<string | null>(null);
+    const [email , setEmail] = useState<string | null>(null);
+    const [password , setPassword] = useState<string | null>(null);
+
+     useEffect(()=>{
+        setName(localStorage.getItem("name"));
+        setLastName(localStorage.getItem("Lastname"));
+        setEmail(localStorage.getItem("email"));
+        setPassword(localStorage.getItem("password"))
+     })
 
     return(
         <div>
